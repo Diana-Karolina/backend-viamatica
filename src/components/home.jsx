17716496ff00d22c76890c3d156a2817ec3de588
Diana-Karolina <ortegadianakarolina@gmail.com>
2023-09-15 16:00:36 -0500

    agregue el back

---
+++ src/components/home.jsx
@@ -0,0 +1,20 @@
+
+import  { useEffect, useState } from "react";
+
+
+const home = () => {
+
+    const [initial, setInitial] = useState ({})
+
+    useEffect(() => {
+        tododsAsegurados()
+    },[])
+
+
+
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
